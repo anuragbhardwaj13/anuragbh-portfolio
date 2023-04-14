d2e2548fd4e20dfd77e1d9cb7f36f97e181121f8
Anurag Bhardwaj <65033601+anuragbhardwaj13@users.noreply.github.com>
2023-04-15 03:00:46 +0530

    new resume updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
             }}
           />
           <div>
-            <a href="https://drive.google.com/file/d/1clT1lNWClfzo7WX55YrcNQGhrSdiBjZI/view?usp=share_link" target="blank">
+            <a href="https://drive.google.com/file/d/1S3vanh0xjz8S_FYW0kRNRMW5glT7nLy9/view?usp=share_link" target="blank">
               My Resume <BsFileEarmarkFill />
             </a>
           </div>
